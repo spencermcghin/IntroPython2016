b132a2d45e87717f4b3dabd9ec0743d23b648464
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-19 16:14:27 -0700

    file languages lab start

--- students/spencer_mcghin/session4/Files_Lab.py
+++ students/spencer_mcghin/session4/Files_Lab.py
@@ -1,9 +1,21 @@
-import os
+import tabulate, collections
 
 
-# Print absolute path of files in current directory #
+# # Print absolute path of files in current directory #
+# def print_path():
+#     for file in os.listdir(os.getcwd()):
+#         print(os.path.abspath(file))
 
-def path_print():
-    for file in os.listdir(os.getcwd()):
-        print(os.path.abspath(file))
+# Read students file and generate languages list #
+def gen_lang():
+    table_set = collections.OrderedDict()
+    doc = open('/Users/SMcGhin/Documents/IntroPython2016/Examples/Session01/students.txt')
+    data = {"Languages": []}
+    for students, languages in doc:
+        print(languages)
+
+gen_lang()
+
+
+
 
