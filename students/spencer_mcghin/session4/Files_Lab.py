292edac2faf5de85b00506fd9704d37282953247
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-20 09:33:03 -0700

    file languages lab start

--- students/spencer_mcghin/session4/Files_Lab.py
+++ students/spencer_mcghin/session4/Files_Lab.py
@@ -7,14 +7,24 @@ import tabulate, collections
 #         print(os.path.abspath(file))
 
 # Read students file and generate languages list #
+# def gen_lang():
+#     table_set = collections.OrderedDict()
+#     doc = open('/Users/SMcGhin/Documents/IntroPython2016/Examples/Session01/students.txt', 'r')
+#     data = {}
+#     for line in doc:
+#         students, languages = line.strip().split(':')
+#         data[students.strip()] = languages.strip()
+#     table_set.update(data)
+
+
 def gen_lang():
-    table_set = collections.OrderedDict()
     doc = open('/Users/SMcGhin/Documents/IntroPython2016/Examples/Session01/students.txt')
-    data = {"Languages": []}
-    for students, languages in doc:
-        print(languages)
+    data = []
+    for line in doc:
+        print(line)
 
 gen_lang()
+
 
 
 
