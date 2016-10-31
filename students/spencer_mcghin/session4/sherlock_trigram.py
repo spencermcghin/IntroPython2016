4be30caabcc4363b69f8794d83262a2058192c94
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-31 15:56:41 -0700

    Finished Files Lab

--- students/spencer_mcghin/session4/sherlock_trigram.py
+++ students/spencer_mcghin/session4/sherlock_trigram.py
@@ -1,8 +1,22 @@
-def trigram():
-    text = open('/Users/SMcGhin/Documents/IntroPython2016/students/spencer_mcghin/session4/sherlock_small.txt', 'r')
-    for words in text:
-        words = words.strip().split('\n')
-    while len(text) >= 2:
+# Imports #
+
+from random import randint
+
+# Open file, get rid of breaks, and return as list #
+
+with open('/Users/SMcGhin/Documents/IntroPython2016/students/spencer_mcghin/session4/sherlock_small.txt', 'r') as text:
+    doc_list = text.read().split()
 
 
-trigram()
+# Input string and generate
+
+def sherlock_trigram():
+    n = input("Please enter a random piece of text.'\n'"
+              "> ")
+    if n in doc_list:
+        print(n + " " + doc_list[randint(doc_list.index(n) - 1, doc_list.index(n) + 1)])
+    else:
+        sherlock_trigram()
+
+
+sherlock_trigram()
