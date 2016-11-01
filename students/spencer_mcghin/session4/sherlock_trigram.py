6d7a3c4d4000e867299e39dcbecc5e22dba21e65
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-01 16:04:15 -0700

    Adjust trigrams lab

--- students/spencer_mcghin/session4/sherlock_trigram.py
+++ students/spencer_mcghin/session4/sherlock_trigram.py
@@ -1,18 +1,21 @@
+# Imports #
+
+
 # Open file, get rid of breaks, and return as list #
 
 with open('/Users/SMcGhin/Documents/IntroPython2016/students/spencer_mcghin/session4/sherlock_small.txt', 'r') as text:
     doc_list = text.read().split()  # Generate List #
-    doc = [" ".join(doc_list)]  # Make one long string #
-
-# Input string and generate
-
-def sherlock_trigram():
-    n = input("Please enter a random piece of text from the story.'\n'"
-              "> ")
-    if n in doc_list:
-        print(n + " " + doc_list.index[n]+1)
-    else:
-        sherlock_trigram()
+    doc = [" ".join(doc_list).strip('--')]  # Make one long string #
 
 
-sherlock_trigram()
+# Validate input against text list and generate following words #
+
+def gen_trigrams():
+    input_text = input("Please enter a text string from the document '\n'"
+                       "> ")
+    input_text = input_text.split()  # convert input_text to list #
+    for i in range(0, len(doc_list)-3):  # iterate through the document and find matches for each part of input_text #
+        if input_text[0] == doc_list[i] and input_text[1] == doc_list[i + 1]:
+            print(doc_list[i + 2])
+
+gen_trigrams()
