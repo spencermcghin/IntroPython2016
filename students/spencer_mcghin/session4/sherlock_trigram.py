a2d5d8247fe7bfcc17433085735a185e0b0fa824
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-01 11:52:37 -0700

    Adjust trigrams lab

--- students/spencer_mcghin/session4/sherlock_trigram.py
+++ students/spencer_mcghin/session4/sherlock_trigram.py
@@ -1,7 +1,3 @@
-# Imports #
-
-from random import randint
-
 # Open file, get rid of breaks, and return as list #
 
 with open('/Users/SMcGhin/Documents/IntroPython2016/students/spencer_mcghin/session4/sherlock_small.txt', 'r') as text:
@@ -14,7 +10,7 @@ def sherlock_trigram():
     n = input("Please enter a random piece of text from the story.'\n'"
               "> ")
     if n in doc_list:
-        print(n + " " + doc_list[randint(doc_list.index(n) - 1, doc_list.index(n) + 1)])
+        print(n + " " + doc_list.index[n]+1)
     else:
         sherlock_trigram()
 
