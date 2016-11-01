c28a43b50f8426cb1b45ed436ec8d02f433108cd
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-10-31 21:32:28 -0700

    Finished Files Lab

--- students/spencer_mcghin/session4/sherlock_trigram.py
+++ students/spencer_mcghin/session4/sherlock_trigram.py
@@ -5,16 +5,16 @@ from random import randint
 # Open file, get rid of breaks, and return as list #
 
 with open('/Users/SMcGhin/Documents/IntroPython2016/students/spencer_mcghin/session4/sherlock_small.txt', 'r') as text:
-    doc_list = text.read().split()
-
+    doc_list = text.read().split()  # Generate List #
+    doc = [" ".join(doc_list)]  # Make one long string #
 
 # Input string and generate
 
 def sherlock_trigram():
-    n = input("Please enter a random piece of text.'\n'"
+    n = input("Please enter a random piece of text from the story.'\n'"
               "> ")
-    if n in doc_list:
-        print(n + " " + doc_list[randint(doc_list.index(n) - 1, doc_list.index(n) + 1)])
+    if n <= doc_list:
+        print(n + " " + doc[randint(doc_list.index(n) - 1, doc_list.index(n) + 1)])
     else:
         sherlock_trigram()
 
