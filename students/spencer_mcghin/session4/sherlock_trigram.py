a2d1b6a07c232820d9c93c29029649d0ca4bf862
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-02 16:29:10 -0700

    Trigrams lab development

--- students/spencer_mcghin/session4/sherlock_trigram.py
+++ students/spencer_mcghin/session4/sherlock_trigram.py
@@ -1,10 +1,11 @@
+import random
+
 # Global Variables #
 
 input_text = input("Please enter a text string from the document '\n'"
                    "> ")
 input_text = input_text.split()  # convert input_text to list #
 tri_list = [input_text]
-final_list = []
 
 # Open file, get rid of breaks, and return as list #
 
@@ -12,21 +13,28 @@ with open('/Users/SMcGhin/Documents/IntroPython2016/students/spencer_mcghin/sess
     doc_list = text.read().split()  # Generate List #
     doc = [" ".join(doc_list).strip('--')]  # Make one long string #
 
+
 # Validate input against text list and generate following words #
+
 
 def gen_trigrams():
     for i in range(0, len(doc_list) - 3):  # iterate through the document and find matches for each part of input_text #
         if input_text[0] == doc_list[i] and input_text[1] == doc_list[i + 1]:
-            tri_list.append([doc_list[i + 2]]) # Append matching values to list object #
-        else:
-            break
-    final_list.append(list(sum(tri_list, [])))  # Add input text and appended values to make one list #
+            tri_list.append([doc_list[i + 2]])  # Append matching values to list object #
+    final_list = (list(sum(tri_list, [])))  # Add input text and appended values to make one list #
+    print(final_list)
 
 # Take Last Two Indexes of gen_trigrams() and perform same operation #
 
+
 def print_story():
+    trigrams = gen_trigrams()
+    first_value = trigrams[-2]
+    second_value = trigrams[-1]
     for i in range(0, len(doc_list) - 3):
-        if gen_trigrams()[-2] == doc_list[i] and gen_trigrams()[-1] == doc_list[i + 1]:
+        if first_value == doc_list[i] and second_value == doc_list[i + 1]:
+            values_list.append(doc_list[i + 2])
+    print(first_value)
+    print(second_value)
 
-
-
+gen_trigrams()
