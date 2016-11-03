1fa961871a35a03f2b7a89b823044723501ea38c
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-03 11:57:06 -0700

    Trigrams lab development

--- students/spencer_mcghin/session4/sherlock_trigram.py
+++ students/spencer_mcghin/session4/sherlock_trigram.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import random
 
 # Global Variables #
@@ -5,7 +7,6 @@ import random
 input_text = input("Please enter a text string from the document '\n'"
                    "> ")
 input_text = input_text.split()  # convert input_text to list #
-tri_list = [input_text]
 matches_list = []  # Container for word(s) that follow input text #
 
 # Open file, get rid of breaks, and return as list #
@@ -22,9 +23,9 @@ def gen_trigrams():
     for i in range(0, len(doc_list) - 3):  # iterate through the document and find matches for each part of input_text #
         if input_text[0] == doc_list[i] and input_text[1] == doc_list[i + 1]:
             matches_list.append(doc_list[i + 2])  # Append matching values to list object #
-    final_list = tri_list + [random.choice(matches_list)]  # Add input text and appended values to make one list #
+    input_text.append(random.choice(matches_list))  # Add input text and appended values to make one list #
     print(matches_list)
-    print(final_list)
+    print(input_text)
 
 
 # Take Last Two Indexes of gen_trigrams() and perform same operation #
