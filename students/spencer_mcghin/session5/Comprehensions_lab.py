e733e705e6761c014fe664c5e452137cca3a72fb
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-06 16:58:17 -0800

    Through Hexadecimal lab

--- students/spencer_mcghin/session5/Comprehensions_lab.py
+++ students/spencer_mcghin/session5/Comprehensions_lab.py
@@ -1,15 +1,31 @@
 # Coding Bat Count Even Numbers #
 
-nums = range(0, 20)
-
-def count_evens(nums):
-
-    num_evens = [number for number in nums if number % 2 == 0]
-    print(num_evens)
-
-
-count_evens(nums)
-
+# nums = range(0, 88)
+#
+# def count_evens(nums):
+#     evens_count = [number for number in nums if number % 2 == 0]
+#     print(len(evens_count))
+#
+#
+# count_evens(nums)
 
 
+# Dict and Set Comprehension #
 
+# food_prefs = {"name": "Spencer",
+#               "city": "Seattle",
+#               "cake": "Funfetti",
+#               "fruit": "Bananas",
+#               "salad": "Macaroni",
+#               "pasta": "Spaghetti"}
+#
+#
+# print("{name} is from {city} and he likes {cake} cake. His favorite fruit is {fruit}, his favorite salad is\n"
+#       "{salad}, and his favorite pasta is {pasta}.".format(**food_prefs))
+
+# Hexadecimal Function #
+# def hex_equiv():
+#     hex_dict = {number: hex(number) for number in range(16)}
+#     print(hex_dict)
+#
+# hex_equiv()
