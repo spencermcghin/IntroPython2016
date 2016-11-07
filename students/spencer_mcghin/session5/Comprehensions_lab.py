6ea719524b8e90f3a66a7854bb1f7e5a132ff2a8
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-06 17:53:10 -0800

    Sets comprehension

--- students/spencer_mcghin/session5/Comprehensions_lab.py
+++ students/spencer_mcghin/session5/Comprehensions_lab.py
@@ -12,20 +12,45 @@
 
 # Dict and Set Comprehension #
 
-# food_prefs = {"name": "Spencer",
-#               "city": "Seattle",
-#               "cake": "Funfetti",
-#               "fruit": "Bananas",
-#               "salad": "Macaroni",
-#               "pasta": "Spaghetti"}
+food_prefs = {"name": "Spencer",
+              "city": "Seattle",
+              "cake": "Funfetti",
+              "fruit": "Bananas",
+              "salad": "Macaroni",
+              "pasta": "Spaghetti"}
+
+
 #
 #
 # print("{name} is from {city} and he likes {cake} cake. His favorite fruit is {fruit}, his favorite salad is\n"
 #       "{salad}, and his favorite pasta is {pasta}.".format(**food_prefs))
 
 # Hexadecimal Function #
+#
 # def hex_equiv():
 #     hex_dict = {number: hex(number) for number in range(16)}
 #     print(hex_dict)
 #
 # hex_equiv()
+
+# A's for Values #
+
+# def a_values():
+#     food_a = {k: v.count('a') for (k, v) in food_prefs.items()}
+#     print(food_a)
+#
+#
+# a_values()
+
+# Sets Comprehensions #
+
+s_2 = set(i for i in range(21) if i % 2 == 0)
+s_3 = set(i for i in range(21) if i % 3 == 0)
+s_4 = set(i for i in range(21) if i % 4 == 0)
+
+def divisors(n):
+    num_sets = set(i for i in range(21) if i % n == 0)
+    print(num_sets)
+
+divisors(4)
+
