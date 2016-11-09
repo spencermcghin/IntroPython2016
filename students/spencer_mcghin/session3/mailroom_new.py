69fc64eff08e9b6e23acb4e5d832c488b6df8924
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-09 10:34:53 -0800

    menu, thank you letter, report, add new user/donation all working

--- students/spencer_mcghin/session3/mailroom_new.py
+++ students/spencer_mcghin/session3/mailroom_new.py
@@ -130,8 +130,8 @@ def add_donor(prompt_donor_input):
 
 
 def add_new_amount(prompt_donor_input):
-    donation_amount = input("What is the donation amount? '\n"
-                            "> ")
+    donation_amount = int(input("What is the donation amount? '\n"
+                                "> "))
     donors[prompt_donor_input].append(donation_amount)
 
 
