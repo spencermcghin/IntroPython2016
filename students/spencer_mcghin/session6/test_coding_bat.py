2bf50afd10040b5214146439d96cf0b57d65f08f
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-10 13:22:15 -0800

    Added coding bat tests

--- students/spencer_mcghin/session6/test_coding_bat.py
+++ students/spencer_mcghin/session6/test_coding_bat.py
@@ -5,20 +5,16 @@ from Coding_Bat_Ex import sorta_sum
 
 
 def test_01():
-    assert a == 10
-    assert b == 9
+    assert sorta_sum(10, 9)
 
 
 def test_02():
-    assert a == 3
-    assert b == 3
+    assert sorta_sum(3, 3)
 
 
 def test_03():
-    assert a == 15
-    assert b == -5
+    assert sorta_sum(100, -81)
 
 
 def test_04():
-    assert a == 1
-    assert b == 1
+    assert sorta_sum(1, 1)
