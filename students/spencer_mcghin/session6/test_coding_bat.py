7419d0cb89b1b0e60e197965c89be58b8d4a6545
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-10 13:15:26 -0800

    Added coding bat tests

---
+++ students/spencer_mcghin/session6/test_coding_bat.py
@@ -0,0 +1,12 @@
+def test_01(a, b):
+    assert a == 10
+    assert b == 9
+
+def test_02(a, b):
+    assert a == 3
+    assert b == 3
+
+def test_03(a, b):
+    assert a == 15
+    assert b == -5
+
