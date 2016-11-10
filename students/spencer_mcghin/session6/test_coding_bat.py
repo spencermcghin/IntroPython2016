548d10a59dd770793c115ef6d530f24928cacf27
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-10 13:20:49 -0800

    Added coding bat tests

--- students/spencer_mcghin/session6/test_coding_bat.py
+++ students/spencer_mcghin/session6/test_coding_bat.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python
 
-def test_01(a, b):
+
+from Coding_Bat_Ex import sorta_sum
+
+
+def test_01():
     assert a == 10
     assert b == 9
 
-def test_02(a, b):
+
+def test_02():
     assert a == 3
     assert b == 3
 
-def test_03(a, b):
+
+def test_03():
     assert a == 15
     assert b == -5
 
+
+def test_04():
+    assert a == 1
+    assert b == 1
