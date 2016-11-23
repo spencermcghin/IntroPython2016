eecbbe6fba54a7b4d030e6c7f61463fd2b7e196d
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-22 20:59:33 -0800

    more circle lab

--- students/spencer_mcghin/session8/Circle.py
+++ students/spencer_mcghin/session8/Circle.py
@@ -1,3 +1,24 @@
+"""
+
+Circle Lab
+
+"""
+
+from math import pi
 
 
-class Circle(radius):
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    @property  # computes on the fly as opposed to storing init value
+    def diameter(self):
+        return self.radius * 2
+
+    @diameter.setter
+    def diameter(self, val):
+        self.radius = val / 2
+
+    @property
+    def area(self):
+        return self.radius ** 2 * math.pi
