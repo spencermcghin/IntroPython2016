05f8f48a8b635ebe22b1b82958f1ff3ed2108348
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-11-22 19:01:33 -0800

    Added more to html_render

--- students/spencer_mcghin/session7/html_render.py
+++ students/spencer_mcghin/session7/html_render.py
@@ -1,25 +1,37 @@
 #!/usr/bin/env python
 
 
+# ToDo - Put text wrapper class here to
+
 class Element(object):
-    """HTML class"""
-    tag = 'html'
-    ind = 4
+    tag = ' '
+    ind = ' '
     print('<!DOCTYPE html>')
 
-    def __init__(self, content=None):
+    def __init__(self, content=None, **kwargs):
         self.content = []
+        self.attributes = kwargs
         if content:
             self.content.append(content)
 
     def append_content(self, content):
-        self.content.append(content)
+        if hasattr(content, 'render'):
+            self.content.append(content)
+        else:
+            self.content.append(TextWrapper(str(content)))  # TextWrapper class here
 
-    def render(self, out_file, ind):
-        out_file.write('<' + self.tag + '>' + '\n')
+    def render(self, out_file, ind=''):
+        out_file.write(ind + '<' + self.tag + '>' + '\n')  # add open_tag instead of '<'...
         for line in self.content:
-            out_file.write((' ' * ind) + line + '\n')
-        out_file.write('</' + self.tag + '>')
+            line.write(out_file, ind + self.ind)
+        out_file.write(ind + '</' + self.tag + '>')  # add close_tag
+
+# ToDo - make method that gets tags and put in render method
+
+
+class Html(Element):
+    """Html subclass"""
+    tag = 'html'
 
 
 class Body(Element):
@@ -34,5 +46,4 @@ class P(Element):
 
 if __name__ == '__main__':
     pass
-
 
