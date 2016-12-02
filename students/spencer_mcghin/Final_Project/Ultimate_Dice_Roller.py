f4232a8ca70c8764d9fd9ae2b6fb37dddeefad4b
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-12-01 21:00:33 -0500

    Finally got my die roller to fuckin' work!!!!!!!

--- students/spencer_mcghin/Final_Project/Ultimate_Dice_Roller.py
+++ students/spencer_mcghin/Final_Project/Ultimate_Dice_Roller.py
@@ -35,17 +35,20 @@ number_of_dice = [str(x) + '.' for x in range(1, len(dice_list) + 1)]
 
 # Classes
 class Dice(object):
+    roll_dict = {}
 
     def __init__(self, number=0, side=None):
         self.number = number
         self.side = side
 
-    @classmethod
+    @staticmethod
     def roll_dice(side, number):
         """ Generate a number of random values based on user input for die_amount in die_amount_selector. """
         rolls = []
-        for _ in range(number + 1):
+        for _ in range(number):
             rolls.append(random.randint(1, side_dict[side]))
+        Dice.roll_dict[side] = rolls
+        print(Dice.roll_dict)
 
 
 # Functions for main program
@@ -70,35 +73,34 @@ def dice_menu():
 
 
 def die_amount_selector(user_input):
-    """ Prompt user for amount of dice to roll and then add to selection_list object. """
     d = Dice()
+    """ Prompt user for amount of dice to roll and then add to selection_list object. """
     try:
         die_amount = int(input("How many would you like to roll? '\n> "))
     except ValueError:
         print("Please enter an appropriate value.")
         die_amount_selector(user_input)
     else:
-        d.side = user_input.strip('.')
-        d.number = die_amount
-        d.add_dice(side=user_input.strip('.'), number=die_amount)
-        roll_more_prompt(d)
+        args = user_input.strip('.'), die_amount
+        d.roll_dice(*args)
+        roll_more_prompt()
 
 
-def roll_more_prompt(d):
+def roll_more_prompt():
     """ Check with user if they would like to roll more / different dice. """
     print("Would you like to roll any additional dice?")
     while True:
         confirm = input("Yes (y) or No (n)? '\n> ")
         if confirm == 'y':
             dice_menu()
+            # ToDo - write roll_dict update function
         elif confirm == 'n':
-            d.roll_dice()
-            # ToDo - Include print die results function here
+            pass
 
 
-def print_die_results(d):
-    """ Print contents of dice_roll_dict to a nice report. """
-    print(tabulate.tabulate(d.get_dice_value(), headers="keys", tablefmt="fancy_grid", numalign="center"))
+# def print_die_results():
+#     """ Print contents of dice_roll_dict to a nice report. """
+#     print(tabulate.tabulate(d.get_dice_value(), headers="keys", tablefmt="fancy_grid", numalign="center"))
 
 if __name__ == '__main__':
     main()
