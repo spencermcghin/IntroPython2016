1ec378849b22690817f555a7aa4a948aa97472ea
spencermcghin <spencer.mcghin@rittmanmead.com>
2016-12-06 20:16:44 -0800

    Created timing context manager

--- students/spencer_mcghin/session10/timing_manager.py
+++ students/spencer_mcghin/session10/timing_manager.py
@@ -4,13 +4,17 @@
 timing context manager
 """
 
+import sys
 import time
 
 
 class Timer:
 
+    def __index__(self, outfile=sys.stdout):
+        self.outfile = outfile
+
     def __enter__(self):
         self.start = time.time()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        print("Elapsed Time:", time.time() - self.start)
+        self.outfile.write("Elapsed Time: {} seconds".format(time.time() - self.start))
